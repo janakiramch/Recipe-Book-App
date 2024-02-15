6a8ddd66fda6eb76ff73da2cb1082f243d26fd6d
Ch JanakiRam <chjanakiram99@gmail.com>
2024-02-15 15:39:18 -0500

    Setting up recipe and shopping list compponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.onceuponachef.com%2Frecipes%2Fbest-pancake-recipe.html&psig=AOvVaw1f_kRiK1Q_giuZ9223GoLV&ust=1708114682566000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKDwoZuVroQDFQAAAAAdAAAAABAJ'
+    ),
+  ];
 }
